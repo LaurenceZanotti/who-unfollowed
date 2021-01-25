61195da7e0dc4b6b05284b6669692e6ed09560c1
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-25 16:00:09 -0300

    Basic program code finished

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import json
 
-followers = None
-following = None
 
-with open('followers.json') as file:
-    followers = file.read()
+def main():
+    followers = None
+    following = None
 
-with open('following.json') as file:
-    following = file.read()
+    with open('followers.json') as file:
+        followers = json.loads(file.read())
 
-print(json.loads(following))
+    with open('following.json') as file:
+        following = json.loads(file.read())
+
+    
+    unfollowed = unfollowedList(followers, following)
+
+    for i in unfollowed:
+        print(i)
+        
+
+def unfollowedList(followers, following):
+    """
+    Return list of people who unfollowed you
+
+    """
+    unfollowed = []
+    for x in followers:
+        if x not in following:
+            unfollowed.append(x)
+
+
+
+    return unfollowed
+                
+main()
 
 
