efab797ce4d31952de3153c2ce7c52fe9a93a659
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-25 18:52:36 -0300

    Added README and implemented CLI arguments

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-import json
+import json, sys
 
 
-def main():
+def main(argv):
     followers = None
     following = None
-
-    with open('followers.json') as file:
-        followers = json.loads(file.read())
-
-    with open('following.json') as file:
-        following = json.loads(file.read())
-
     
-    unfollowed = unfollowedList(followers, following)
+    if len(argv) != 3:
+        print("Missing arguments. Usage: main.py followers.txt following.txt -u")
+        sys.exit(1)
 
-    for i in unfollowed:
-        print(i)
+    try:
+        with open(argv[1]) as file:
+            followers = json.loads(file.read())
+    except FileNotFoundError:
+        print(f'File {argv[1]} doesn\'t exist. Was it typed correctly or is it in the same directory?')
+        sys.exit(1)
+
+    try:
+        with open(argv[2]) as file:
+            following = json.loads(file.read())
+    except FileNotFoundError:
+        print(f'File {argv[2]} doesn\'t exist. Was it typed correctly or is it in the same directory?')
+        sys.exit(1)
+
+    # Retrieve list of accounts that are not following back
+    if "-u" in argv:
+        unfollowed = unfollowedList(followers, following)
+        print("People who doesn't follow you back:\n")
+        for i in unfollowed:
+            print(i)
+    else:
+            print("Missing argument. Usage: main.py [-u] for accounts that unfollowed you.")
+
+    if "-b" in argv:
+        unfollowed = unfollowedList(following, followers)
+        print("People who you don't follow back:\n")
+        for i in unfollowed:
+            print(i)
         
 
 def unfollowedList(followers, following):
@@ -24,14 +45,15 @@ def unfollowedList(followers, following):
 
     """
     unfollowed = []
-    for x in followers:
-        if x not in following:
+    for x in following:
+        if x not in followers:
             unfollowed.append(x)
 
 
 
     return unfollowed
-                
-main()
+
+
+main(sys.argv)
 
 
